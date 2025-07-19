38c87b798dff58ec021a9547300aae1f87ab4588
Maxim <maxim.khalamov@gmail.com>
2025-07-19 20:08:06 +0300

    Initial commit

---
+++ common/src/main/java/com/quazarx/infinitewar/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.quazarx.infinitewar;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "infinitewar";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
